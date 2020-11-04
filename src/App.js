f81acca1ee6b384bb9f59f71bcb601e3ac3a66f8
Kaushik Murali <kaushik-murali@agentz.ai>
2020-11-04 10:26:01 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import PhoneInput from 'react-phone-number-input';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function App() {
+    const [value, setValue] = useState('');
+    return (
+      <div className="App">
+        <PhoneInput
+          international
+          countryCallingCodeEditable={false}
+          defaultCountry="US"
+          value={value}
+          onChange={setValue}
+        />
+      </div>
+    );
+  }
 
 export default App;
